6d4a408264b6c220dd3c7002dffc762a11c69e89
mohit <vpm3061@gmail.com>
2026-03-02 15:32:58 +0530

    fix: correct ThemeProvider import in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { themeProvider } from '@/components/layout/themeProvider'
+import { ThemeProvider } from '@/components/layout/ThemeProvider'
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
 
